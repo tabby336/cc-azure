4818e3027a9d283a0fc6555411d2c02574bb08a2
tabby336 <ioana.bogdan25@gmail.com>
2017-05-19 23:31:24 +0300

    Testing

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 from flask import Flask, jsonify, abort, request, make_response, url_for
 
-from xml.etree import ElementTree
-from auth import AzureAuthClient
-import requests
+# from xml.etree import ElementTree
+# from auth import AzureAuthClient
+# import requests
 
 app = Flask(__name__, static_url_path = "", static_folder = "static")
 
-client_secret = '41578f022a0841ee83364daf3e0e571c'
-auth_client = AzureAuthClient(client_secret)
-bearer_token = 'Bearer ' + auth_client.get_access_token()
+# client_secret = '41578f022a0841ee83364daf3e0e571c'
+# auth_client = AzureAuthClient(client_secret)
+# bearer_token = 'Bearer ' + auth_client.get_access_token()
 
 @app.route('/')
 def index():
@@ -23,11 +23,7 @@ def translate():
 
     # Call to Microsoft Translator Service
     headers = {"Authorization ": bearer_token}
-<<<<<<< HEAD
-    translateUrl = "http://api.microsofttranslator.com/v2/Http.svc/Translate?text={}&to={}".format(request.json["text"], request.json["lang"])
-=======
     translateUrl = "https://api.microsofttranslator.com/v2/Http.svc/Translate?text={}&to={}".format(request.json["text"], request.json["lang"])
->>>>>>> working
 
     translationData = requests.get(translateUrl, headers = headers)
     # parse xml return values
