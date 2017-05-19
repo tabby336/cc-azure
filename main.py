546ee7a665cc7ba2e73c62da47214508ee68c6c6
tabby336 <ioana.bogdan25@gmail.com>
2017-05-19 23:27:31 +0300

    Testing

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ def translate():
 
     # Call to Microsoft Translator Service
     headers = {"Authorization ": bearer_token}
-    translateUrl = "http://api.microsofttranslator.com/v2/Http.svc/Translate?text={}&to={}".format(request.json["text"], request.json["lang"])
+    translateUrl = "https://api.microsofttranslator.com/v2/Http.svc/Translate?text={}&to={}".format(request.json["text"], request.json["lang"])
 
     translationData = requests.get(translateUrl, headers = headers)
     # parse xml return values
