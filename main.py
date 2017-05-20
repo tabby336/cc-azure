cc1ca16f4aafe7a7f786d62ec20cea6acef1b2d7
tabby336 <ioana.bogdan25@gmail.com>
2017-05-20 10:08:26 +0300

    testing

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ app = Flask(__name__, static_url_path = "", static_folder = "static")
 
 # client_secret = '41578f022a0841ee83364daf3e0e571c'
 # auth_client = AzureAuthClient(client_secret)
-# bearer_token = 'Bearer ' + auth_client.get_access_token()
+bearer_token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzY29wZSI6Imh0dHBzOi8vYXBpLm1pY3Jvc29mdHRyYW5zbGF0b3IuY29tLyIsInN1YnNjcmlwdGlvbi1pZCI6ImQxNWY3ODMwMjU4ZDQxZWQ5ZjcwODIzZWZlNzczYzI1IiwicHJvZHVjdC1pZCI6IlRleHRUcmFuc2xhdG9yLkYwIiwiY29nbml0aXZlLXNlcnZpY2VzLWVuZHBvaW50IjoiaHR0cHM6Ly9hcGkuY29nbml0aXZlLm1pY3Jvc29mdC5jb20vaW50ZXJuYWwvdjEuMC8iLCJhenVyZS1yZXNvdXJjZS1pZCI6Ii9zdWJzY3JpcHRpb25zL2ZlYjhmMjcyLWI1ZTItNGJhMC1hZDk2LTViZTY1Y2Y5MzJiYS9yZXNvdXJjZUdyb3Vwcy9teVJlc291cmNlR3JvdXAvcHJvdmlkZXJzL01pY3Jvc29mdC5Db2duaXRpdmVTZXJ2aWNlcy9hY2NvdW50cy9jYy1henVyZS10cmFuc2xhdGUtdGV4dCIsImlzcyI6InVybjptcy5jb2duaXRpdmVzZXJ2aWNlcyIsImF1ZCI6InVybjptcy5taWNyb3NvZnR0cmFuc2xhdG9yIiwiZXhwIjoxNDk1MjY0NjYyfQ.cNlfPgjrYMRsqNo8B3Ujq9xoBL4sTVX8HawCPhF-_z8'
 
 @app.route('/')
 def index():
@@ -35,4 +35,5 @@ def translate():
     return jsonify({"text": translation.text})
 
 if __name__ == '__main__':
+    print bearer_token
     app.run()
